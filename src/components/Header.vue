11d2f6a1dd39f81b89460d34c26485ba89693254
Donatella7 <campaniellodonatella@gmail.com>
2022-03-28 22:07:57 +0200

    components nested

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header class="container-fluid">
+        <div class="row p-4">
+            <div class="col-12">
+                <h2>ciao</h2>
+                <Paragraph/>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+import Paragraph from "./Paragraph.vue"
+
+export default {
+    name:"HeaderTest",
+    components:{
+        Paragraph,
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Paragraph.vue
@@ -0,0 +1,15 @@
+<template>
+    <p>
+        sono un p
+    </p>
+</template>
+
+<script>
+export default {
+    name:"ParagraphTest",
+}
+</script>
+
+<style>
+
+</style>
